ae2259e220e44a37e2debb2de5c71a195094117a
arpita330 <arpitanaha721@gmail.com>
2026-02-23 08:02:49 +0530

    Create crypto.js

---
+++ utils/crypto.js
@@ -0,0 +1,28 @@
+const crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+
+const key = crypto
+  .createHash("sha256")
+  .update(process.env.AES_SECRET)
+  .digest();
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted;
+}
+
+function decrypt(data) {
+  const parts = data.split(":");
+  const iv = Buffer.from(parts.shift(), "hex");
+  const encryptedText = parts.join(":");
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encryptedText, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt };
